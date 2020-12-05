0b0cb85a863edb66d2b647385fd75c933771285d
yurikrupnik <krupnik.yuri@gmail.com>
2020-12-05 12:23:37 +0200

    docker updates

--- packages/servers/webserver1/src/routes/Dashboard/Dashboard.tsx
+++ packages/servers/webserver1/src/routes/Dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+// import Checkbox from "@material-ui/core/Checkbox";
 // import PropTypes from 'prop-types';
 
 // interface Props {
