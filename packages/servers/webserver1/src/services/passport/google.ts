5b804f62c75287ca4fa2d138026f4a79684e493f
yurikrupnik <krupnik.yuri@gmail.com>
2020-12-28 23:01:18 +0200

    publish

--- packages/servers/webserver1/src/services/passport/google.ts
+++ packages/servers/webserver1/src/services/passport/google.ts
@@ -4,7 +4,7 @@ import { socialNetworkStrategy } from "./utils";
 export default new Strategy(
     {
         clientID: "813573427784-u6okgh1ituqakgv729uvuftpdmm38hs5.apps.googleusercontent.com",
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET || "KyxjO9p4yA4xhz8UVpIxyu82",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET || "qYhchpZVxQqkr2y6aqfO8_-6",
         callbackURL: "/auth/google/callback",
     },
     socialNetworkStrategy
